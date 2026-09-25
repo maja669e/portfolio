5bd53009b3bed92ecb0a9babbd4c1417dbae3e67
maja669e <maja.bijedic@live.dk>
2026-09-25 21:30:43 +0200

    Added burger menu for tailwind mobile

--- tailwind_portfolio/src/main.js
+++ tailwind_portfolio/src/main.js
@@ -2,3 +2,12 @@ import './style.css'
 
 // Get the current year
 document.getElementById('year').textContent = new Date().getFullYear()
+
+// Get the burger button and menu
+const menuButton = document.getElementById('menu-button')
+const menu = document.getElementById('menu')
+
+// Open and close the menu 
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('hidden')
+})
